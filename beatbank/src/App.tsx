14770eb37f2ca943c3a4d558369d9255154efb04
ParkerTonra <parkertonra@gmail.com>
2024-09-18 21:33:37 -0600

    merge splash screen + dev

--- beatbank/src/App.tsx
+++ beatbank/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import { open } from '@tauri-apps/api/dialog';
 import "./App.css";
 import "./Main.css";
 import { SplashScreen } from "./components/SplashScreen";
@@ -8,52 +7,38 @@ import { SplashScreen } from "./components/SplashScreen";
 function App() {
   const [showSplashScreen, setShowSplashScreen] = useState(true);
 
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState<string | string[] | null>(null);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const handleFileUpload = async () => {
+    try {
+      const filePath = await open({
+        directory: false,
+        multiple: true,
+        filters: [{
+          name: 'Audio Files',
+          extensions: ['flac', 'wav', 'mp3', 'ogg', 'm4a', 'aac', 'aiff', 'wma']
+        }]
+      });
+
+      if (filePath) {
+        // if just one file is selected, setSelectedFile to that file, otherwise make a string array of all the files
+        setSelectedFile(filePath);
+        console.log("Selected file:", filePath);
+      }
+    } catch (error) {
+      console.error("Error selecting file:", error);
+    }
+  };
 
   if (showSplashScreen) {
-    return <SplashScreen closeSplashScreen={() => setShowSplashScreen(false)} />
+    return <SplashScreen closeSplashScreen={() => setShowSplashScreen(false)} />;
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <h1>Welcome to Beatbank!</h1>
+      <button onClick={handleFileUpload}>Upload a beat</button>
+      {selectedFile && <p>Selected file: {selectedFile}</p>}
     </div>
   );
 }
