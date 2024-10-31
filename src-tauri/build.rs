b95913e74483e450c50ff1a54691fbe2daa1ab71
BudLight-Year <totallysweethobo@gmail.com>
2024-10-30 19:57:36 -0600

    Initial commit

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    tauri_build::build()
+    println!("Expected binary path: {:?}", 
+    std::env::current_dir().unwrap().join("dist/audio_analyzer.exe"));
+    tauri_build::build();
 }
