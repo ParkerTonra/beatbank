e8adf1737dac15370c6e246ce09e5c82e13ff098
daskimmel <david.soth-kimmel@everstream.ai>
2024-11-17 17:33:37 -0700

    Spacing

--- src/bindings.ts
+++ src/bindings.ts
@@ -1,33 +1,33 @@
 // TODO: update to new schema
 export type Beat = {
-    id: number;
-    title: string;
-    artist?: string;
-    album?: string;
-    genre?: string;
-    year?: number;
-    track_number?: number;
-    duration?: number;
-    composer?: string;
-    lyricist?: string;
-    cover_art?: string;
-    comments?: string;
-    file_path: string;
-    bpm?: number | undefined;
-    musical_key?: string;
-  };
+  id: number;
+  title: string;
+  artist?: string;
+  album?: string;
+  genre?: string;
+  year?: number;
+  track_number?: number;
+  duration?: number;
+  composer?: string;
+  lyricist?: string;
+  cover_art?: string;
+  comments?: string;
+  file_path: string;
+  bpm?: number | undefined;
+  musical_key?: string;
+};
 
 export type BeatCollection = {
-    id: number;
-    set_name: string;
-    venue?: string;
-    city?: string;
-    state_name?: string;
-    date_played?: string;
-    date_created?: string;
-  };
+  id: number;
+  set_name: string;
+  venue?: string;
+  city?: string;
+  state_name?: string;
+  date_played?: string;
+  date_created?: string;
+};
 
-  export type ColumnVis = {
+export type ColumnVis = {
   title: boolean;
   bpm: boolean;
   musical_key: boolean;
@@ -41,7 +41,7 @@ export type BeatCollection = {
 export type EditThisBeat = {
   id: number;
   title: string;
-  bpm?: number; 
+  bpm?: number;
   musical_key?: string;
   duration?: number;
   artist?: string;
